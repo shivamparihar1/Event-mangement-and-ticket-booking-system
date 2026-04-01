3e9d66ab9ae23ae83391bc7971cd8d274ec48b6e
Shivam Parihar <thakurshivamparihar83@gmail.com>
2026-04-01 19:48:06 +0530

    Login feature completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,5 @@
+// controllers/authController.js
+
 const User = require("../models/User");
 
 const registerUser = async (req, res) => {
@@ -30,4 +32,31 @@ const registerUser = async (req, res) => {
   });
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (user.password !== password) {
+    return res.status(400).json({ message: "Wrong password" });
+  }
+
+  res.status(200).json({
+    message: "Login successful",
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  });
+};
+
+module.exports = { registerUser, loginUser };
