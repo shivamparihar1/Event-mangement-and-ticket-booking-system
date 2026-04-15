2532eb1eaf27ac5a09a9931b1927fb1cbe42055e
Shivam Parihar <thakurshivamparihar83@gmail.com>
2026-04-15 11:25:48 +0530

    feat: add login page

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
